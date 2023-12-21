d964683a382af4a3b99b19d92686590bf4594ce4
Joshua Perrett <jperrett256@gmail.com>
2023-12-21 00:01:21 +0000

    removing bat_callers directory from PATH in scripts
    This is to avoid recursive calls; vcvarsall both prepends and appends
    to PATH, and clang-cl.exe is an example of something that ends up being
    hidden by the clang-cl.exe found in the bat_callers directory.

--- output/clang-cl.bat
+++ output/clang-cl.bat
@@ -3,4 +3,8 @@ setlocal
 rem NOTE comment/uncomment for 32/64 bit
 rem call "%~dp0_vcvarsall32.bat"
 call "%~dp0_vcvarsall64.bat"
+
+rem NOTE removes directory of executables that call these bat files
+rem (substring needs to match exactly, modify as needed)
+set PATH=%PATH:C:\Utilities\bat_callers;=%
 call "clang-cl.exe" %*
