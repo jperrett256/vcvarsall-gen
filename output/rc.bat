67bf48e7d0c7a4ed8ea8b71d8343776fed0db0a1
Joshua Perrett <jperrett256@gmail.com>
2024-03-26 16:08:51 +0000

    added bat files: rc

---
+++ output/rc.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+rem NOTE comment/uncomment for 32/64 bit
+rem call "%~dp0_vcvarsall32.bat"
+call "%~dp0_vcvarsall64.bat"
+
+rem NOTE removes directory of executables that call these bat files
+rem (substring needs to match exactly, modify as needed)
+set PATH=%PATH:C:\Utilities\bat_callers;=%
+call "rc.exe" %*
